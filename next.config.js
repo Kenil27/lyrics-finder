84edba87c2cb17a2fe38433c190bc361b7337ffe
Kenil Shah <kenilshah93@gmail.com>
2019-07-30 21:58:21 +0530

    config changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const withCSS = require('@zeit/next-css')
 /* Without CSS Modules, with PostCSS */
 
-module.exports = withCSS()
+module.exports = withCSS({
+    assetPrefix: isProd ? '/lyrics-finder' : '',
+})
